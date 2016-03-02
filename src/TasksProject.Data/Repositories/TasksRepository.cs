14302709d9a78e2ca0a8d5187bb049b1f02c7e41
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-02 20:15:55 +0000

    Add Repository method stubs

--- src/TasksProject.Data/Repositories/TasksRepository.cs
+++ src/TasksProject.Data/Repositories/TasksRepository.cs
@@ -1,8 +1,38 @@
 ﻿namespace TasksProject.Data.Repositories
 {
     using Shared.Interfaces.Repositories;
+    using System;
 
     internal sealed class TasksRepository : ITasksRepository
     {
+        public void AddTask(string title, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTask(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarkComplete(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurgeDeleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RestoreTask(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTask(Guid taskId, string title, string description)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/TasksProject.Shared/Interfaces/Repositories/ITasksRepository.cs
+++ src/TasksProject.Shared/Interfaces/Repositories/ITasksRepository.cs
@@ -1,6 +1,20 @@
 ﻿namespace TasksProject.Shared.Interfaces.Repositories
 {
+    using System;
+    using Attributes;
+
     public interface ITasksRepository
     {
+        void AddTask(string title, string description);
+
+        bool DeleteTask(Guid taskId);
+
+        bool MarkComplete(Guid taskId);
+
+        void PurgeDeleted();
+
+        bool RestoreTask(Guid taskId);
+
+        bool UpdateTask(Guid taskId, [NotNull] string title, string description = null);
     }
 }
