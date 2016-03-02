e4a30ff38149eb21267bac837b7871d159eeee27
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-02 17:00:26 +0000

    Add data store containers

---
+++ src/TasksProject.Data/DataStore/TaskStore.cs
@@ -0,0 +1,50 @@
+﻿namespace TasksProject.Data.DataStore
+{
+    using System.Collections.Generic;
+    using Entities;
+    using Interfaces.DataStore;
+
+    internal sealed class TaskStore : ITaskStore
+    {
+        public List<Task> InProgressTasks
+        {
+            get
+            {
+                return _inProgressTasks ?? (_inProgressTasks = new List<Task>());
+            }
+            set
+            {
+                _inProgressTasks = value;
+            }
+        }
+
+        private List<Task> _inProgressTasks;
+
+        public List<DeletedTask> DeletedTasks {
+            get
+            {
+                return _deletedProgressTasks ?? (_deletedProgressTasks = new List<DeletedTask>());
+            }
+            set
+            {
+                _deletedProgressTasks = value;
+            }
+        }
+
+        private List<DeletedTask> _deletedProgressTasks;
+
+        public List<CompletedTask> CompletedTasks
+        {
+            get
+            {
+                return _completedTasks ?? (_completedTasks = new List<CompletedTask>());
+            }
+            set
+            {
+                _completedTasks = value;
+            }
+        }
+
+        private List<CompletedTask> _completedTasks;
+    }
+}
---
+++ src/TasksProject.Data/Interfaces/DataStore/ITaskStore.cs
@@ -0,0 +1,14 @@
+﻿namespace TasksProject.Data.Interfaces.DataStore
+{
+    using System.Collections.Generic;
+    using Entities;
+
+    internal interface ITaskStore
+    {
+        List<Task> InProgressTasks { get; set; }
+
+        List<DeletedTask> DeletedTasks { get; set; }
+
+        List<CompletedTask> CompletedTasks { get; set; }
+    }
+}
