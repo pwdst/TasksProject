b61f9fbd56b62b5b1c0cbcdf4a9e93c9832bed1e
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-03 23:06:24 +0000

    Added Request extension methods

---
+++ src/TasksProject/Extensions/RequestExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace TasksProject.Extensions
+{
+    using System;
+    using System.Linq;
+    using Microsoft.AspNet.Http;
+
+    public static class RequestExtensions
+    {
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            const string requestedWithHeaderkey = "X-Requested-With";
+
+            var requestedWithHeader = request.Headers.Keys.FirstOrDefault(k => k.Equals(requestedWithHeaderkey, StringComparison.OrdinalIgnoreCase));
+
+            if (requestedWithHeader == null)
+            {
+                return false;
+            }
+
+            const string xmlHttpRequest = "XMLHttpRequest";
+
+            return requestedWithHeader.Equals(xmlHttpRequest, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
