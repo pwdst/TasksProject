b48f3afd5d42718af56d38ba5c75961bdd2c58ab
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-03 22:08:24 +0000

    Added Projection classes

---
+++ src/TasksProject.Shared/Projections/DeletedTask.cs
@@ -0,0 +1,31 @@
+﻿namespace TasksProject.Shared.Projections
+{
+    using System;
+    using Attributes;
+
+    public class DeletedTask
+    {
+        public DeletedTask(Guid id, [NotNull] string title, DateTime createdOn, string description, DateTime deletedOn)
+        {
+            Id = id;
+
+            Title = title;
+
+            CreatedOn = createdOn;
+
+            Description = description;
+
+            DeletedOn = deletedOn;
+        }
+
+        public Guid Id { get; }
+
+        public string Title { get; }
+
+        public DateTime CreatedOn { get; }
+
+        public string Description { get; }
+
+        public DateTime DeletedOn { get; }
+    }
+}
---
+++ src/TasksProject.Shared/Projections/Task.cs
@@ -0,0 +1,44 @@
+﻿namespace TasksProject.Shared.Projections
+{
+    using System;
+    using Attributes;
+
+    public class Task
+    {
+        public Task(Guid id, [NotNull] string title, DateTime createdOn, string description)
+        {
+            Id = id;
+
+            Title = title;
+
+            CreatedOn = createdOn;
+
+            Description = description;
+        }
+
+        public Task(Guid id, [NotNull] string title, DateTime createdOn, string description, DateTime completedOn)
+        {
+            Id = id;
+
+            Title = title;
+
+            CreatedOn = createdOn;
+
+            Description = description;
+
+            CompletedOn = completedOn;
+        }
+
+        public Guid Id { get; }
+
+        public string Title { get; }
+
+        public DateTime CreatedOn { get; }
+
+        public string Description { get; }
+
+        public DateTime? CompletedOn { get; }
+
+        public bool IsCompleted => CompletedOn.HasValue;
+    }
+}
