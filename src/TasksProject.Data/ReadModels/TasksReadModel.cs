5e28efc9d2c5d8273c5bdef110ad0ac5f2177097
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-04 00:04:55 +0000

    Add TaskStore to TaskReadModel

--- src/TasksProject.Data/ReadModels/TasksReadModel.cs
+++ src/TasksProject.Data/ReadModels/TasksReadModel.cs
@@ -1,11 +1,19 @@
 ﻿namespace TasksProject.Data.ReadModels
 {
     using System.Collections.Generic;
+    using Interfaces.DataStore;
     using Shared.Interfaces.ReadModels;
     using Shared.Projections;
 
     internal class TasksReadModel : ITasksReadModel
     {
+        private readonly ITaskStore _taskStore;
+
+        public TasksReadModel(ITaskStore taskStore)
+        {
+            _taskStore = taskStore;
+        }
+
         public int DeletedTaskCount()
         {
             throw new System.NotImplementedException();
