ad4edeb22caa939d92ef8e61e5ccbedea924288b
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-06 21:52:44 +0000

    Added new mapping classes

---
+++ src/TasksProject.Data/Mapping/DeletedTaskMapping.cs
@@ -0,0 +1,12 @@
+﻿namespace TasksProject.Data.Mapping
+{
+    using DeletedTask = Shared.Projections.DeletedTask;
+
+    internal static class DeletedTaskMapping
+    {
+        internal static DeletedTask Map(Entities.DeletedTask deletedTask)
+        {
+            return new DeletedTask(deletedTask.Id, deletedTask.Title, deletedTask.CreatedOn, deletedTask.Description, deletedTask.DeletedOn);
+        }
+    }
+}
---
+++ src/TasksProject.Data/Mapping/TaskMapping.cs
@@ -0,0 +1,18 @@
+﻿namespace TasksProject.Data.Mapping
+{
+    using Task = Shared.Projections.Task;
+
+    internal static class TaskMapping
+    {
+        internal static Task Map(Entities.Task task)
+        {
+            return new Task(task.Id, task.Title, task.CreatedOn, task.Description);
+        }
+
+        public static Task Map(Entities.CompletedTask completedTask)
+        {
+            return new Task(completedTask.Id, completedTask.Title, completedTask.CreatedOn,
+                completedTask.Description, completedTask.CompletedOn);
+        }
+    }
+}
