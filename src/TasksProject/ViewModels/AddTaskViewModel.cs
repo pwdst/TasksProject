6a9fb48c25f610ea39a0f65b67095c6facc6c8b1
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-07 09:16:24 +0000

    Add maximum length constraints for Add Task form properties

--- src/TasksProject/ViewModels/AddTaskViewModel.cs
+++ src/TasksProject/ViewModels/AddTaskViewModel.cs
@@ -4,9 +4,14 @@
 
     public class AddTaskViewModel
     {
-        [Required]
+        public const int TitleMaxLength = 200;
+
+        public const int DescriptionMaxLength = 5000;
+
+        [MaxLength(TitleMaxLength), Required]
         public string Title { get; set; }
 
+        [MaxLength(DescriptionMaxLength)]
         public string Description { get; set; }
     }
 }
